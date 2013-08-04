9efa1c54c3039775b76c19dccbe5fdacaef4a8a5
Anna Hendre <you@example.com>
2013-08-04 16:44:08 +0100

    fixed cancan ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,19 +4,16 @@ class Ability
 
   def initialize(user)
     user ||= User.new 
-    if user.role? "registered"
+    if user.role? "admin"
+      can :manage, :all
+    elsif user.role? "registered"
       can :read, :all
       can :create, Comment
-      can :destroy, Comment do |c|
-        (c.photo.user_id == user_id if (c.photo and c.photo.user)) || c.user_id ==user_id
       can :manage, Album 
-      can :manage, Photo
-      end
+      can :manage, Photo  
       can :manage, User do |u|
         u.id == user.id
       end
-    elsif user.role? "admin"
-      can :manage, :all
     else
       can :read, :all
       can :create, User
