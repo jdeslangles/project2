40ea691602a7b66445d276ac52f97187b0946030
Anna Hendre <you@example.com>
2013-08-05 15:38:18 +0100

    photo album edit buttons not working for cancan ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,12 +7,15 @@ class Ability
     if user.role? "admin"
       can :manage, :all
     elsif user.role? "registered"
-      can :read, :all
-      can :create, Comment
-      can :manage, Album 
       can :manage, User do |u|
         u.id == user.id
       end
+      can :vote, Photo
+      can :read, :all
+      can :create, Comment
+      can :destroy, Comment
+      can :manage, Album 
+      can :manage, Photo
     else
       can :read, :all
       can :photo_wall, Photo
