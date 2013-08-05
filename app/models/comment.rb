a9b971bbaf009f586bc82a9602b870d4af71cefd
bj <britneyjo.ludkowski@gmail.com>
2013-08-05 14:23:13 +0100

    changed validation for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ActiveRecord::Base
 
   attr_accessible :content, :user_id, :photo_id
 
-  validates :content, presence: true, length:{in:(5..500)}
+  validates :content, presence: true, length:{maximum:500}
 
   belongs_to :user
   belongs_to :photo
