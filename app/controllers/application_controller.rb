334160fa4a61c7cb8900a1761b2fbd25da4b75ba
Anna Hendre <you@example.com>
2013-08-05 14:59:04 +0100

    fixed voting error when not logged in as a user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
 	protect_from_forgery
 
+	rescue_from CanCan::AccessDenied do |exception|
+  	redirect_to login_path, alert: "You shall not pass!"
+	end
+
 
 	helper_method:current_user
 	def current_user
