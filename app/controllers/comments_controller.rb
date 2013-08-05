95efeb7bca4f0983f2dca59b7db171ff72e4a427
bj <britneyjo.ludkowski@gmail.com>
2013-08-05 17:41:28 +0100

    added delete link on comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -15,7 +15,7 @@ class CommentsController < ApplicationController
   # GET /comments/1.json
   def show
     @comment = Comment.find(params[:id])
-    
+
 
     respond_to do |format|
       format.html # show.html.erb
@@ -37,7 +37,7 @@ class CommentsController < ApplicationController
   # GET /comments/1/edit
   def edit
     @comment = Comment.find(params[:id])
-    
+
   end
 
   # POST /comments
@@ -46,7 +46,7 @@ class CommentsController < ApplicationController
     params[:comment][:photo_id] = params[:id]
     params[:comment][:user_id] = current_user.id
     @comment = Comment.new(params[:comment])
-    
+
 
 
     respond_to do |format|
@@ -65,7 +65,7 @@ class CommentsController < ApplicationController
   # PUT /comments/1.json
   def update
     @comment = Comment.find(params[:id])
-    
+
 
     respond_to do |format|
       if @comment.update_attributes(params[:comment])
@@ -82,11 +82,11 @@ class CommentsController < ApplicationController
   # DELETE /comments/1.json
   def destroy
     @comment = Comment.find(params[:id])
-    
+    photo = @comment.photo
     @comment.destroy
 
     respond_to do |format|
-      format.html { redirect_to comments_url }
+      format.html { redirect_to user_album_photo_path(photo.album.user, photo.album, photo) }
       format.json { head :no_content }
     end
   end
