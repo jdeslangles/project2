05a031fe52199d184f9dfee362657d2edebc65f5
bj <britneyjo.ludkowski@gmail.com>
2013-08-07 14:56:55 +0100

    added error flash message for search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,7 +4,15 @@ class SearchController < ApplicationController
       @photos = Photo.where('name like :name', name: "%#{params[:q]}%")
       @album = Album.where('name like :name', name: "%#{params[:q]}%")
       @users = User.where('username like :username', username: "%#{params[:q]}%")
-
+      if params[:q].empty?
+        flash[:error] = "No matches for #{params[:q]}."
+      else
+        flash[:error] = nil
+      end
+    else
+      @photos=[]
+      @albums = []
+      @users = []
     end
   end
 
