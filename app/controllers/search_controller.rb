9bb2a43b7a638308fed9bddf9a502d94fc25bb23
bj <britneyjo.ludkowski@gmail.com>
2013-08-08 14:35:46 +0100

    fixed empty search bug

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,13 @@
 class SearchController < ApplicationController
   def index
-    if params[:q]
+    if params[:q].blank?
+      flash[:notice] = "Your search query was empty."
+      redirect_to photo_wall_path
+    else
       search = params[:q].to_s.downcase
       @photos = Photo.where('LOWER(name) like :search OR LOWER(description) like :search', search: "%#{search}%")
       @albums = Album.where('LOWER(name) like :search OR LOWER(description) like :search', search: "%#{search}%")
       @users = User.where('LOWER(username) like :search OR LOWER(first_name) like :search OR LOWER(last_name) like :search OR LOWER(biography) like :search OR LOWER(location) like :search', search: "%#{search}%")
-    else
-      @photos=[]
-      @albums = []
-      @users = []
     end
   end
 
