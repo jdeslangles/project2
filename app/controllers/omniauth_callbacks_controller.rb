231f2e33677fdfcb90f10d73a9b539f60582074b
bj <britneyjo.ludkowski@gmail.com>
2013-08-09 10:07:08 +0100

    removed binding pry from google auth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -6,8 +6,7 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
       flash.notice = "Signed in Through Google!"
       sign_in_and_redirect user
     else
-      binding.pry
-      session["devise.user_attributes"] = user.attributes 
+      session["devise.user_attributes"] = user.attributes
       flash.notice = "Problem creating account"
       redirect_to new_user_registration_url
     end
