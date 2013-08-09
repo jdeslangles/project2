a18c1e917ab5b5bc4276ee06a61906cfbf909371
jdeslangles <julien.deslangles@gmail.com>
2013-08-09 11:27:56 +0100

    changed local photos controller

--- app/controllers/local_photos_controller.rb
+++ app/controllers/local_photos_controller.rb
@@ -2,13 +2,18 @@ class LocalPhotosController < ApplicationController
 
   def show
     @photo = Photo.find(params[:id])
-      authorize! :show, @photo
+    authorize! :show, @photo
 
-      respond_to do |format|
-        format.html do
-          send_file @photo.photo_picture.file.path, :disposition => "inline"
-
+    respond_to do |format|
+      format.html do
+        if @photo.photo_picture.file.respond_to?(:url)
+          url = @photo.photo_picture.file.try(:url)
+          data = open(url).read
+          send_data data, :disposition => 'inline'
+        else
+          send_file @photo.photo_picture.file.path, :disposition => 'inline'
         end
+      end
     end
   end
 end
